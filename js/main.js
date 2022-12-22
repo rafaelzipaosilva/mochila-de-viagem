f7a39b395818f2ad00200174d63ac308043d89d6
Rafael "Zipão" Silva <zipao.silva@gmail.com>
2022-12-22 18:44:44 -0300

    aula3

--- js/main.js
+++ js/main.js
@@ -1,36 +1,49 @@
-const form = document.getElementById("novoItem")
-const lista = document.getElementById("lista")
-const itens = []
+// Operador lógico que retorna com dados salvos, ou string vazia, utilizando localStorage.getItem, modificando o valor de `string` com JSON.parse()
 
-form.addEventListener("submit", (evento) => {
+const form = document.getElementById("novoItem") 
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []     
+
+// Uso do forEach para que todos os itens já escritos na lista sejam mantidos ao atualizar a página 
+
+itens.forEach( (elemento) => {    
+    criaElemento(elemento)
+} )    
+
+// Refatoração do addEventListener para receber as funções extras da função criaElemento
+
+form.addEventListener("submit", (evento) => {  
     evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value, quantidade.value)
+    const itemAtual = {
+    "nome": nome.value,
+    "quantidade": quantidade.value
+    }
+
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 
     nome.value = ""
     quantidade.value = ""
 })
 
-function criaElemento(nome, quantidade) {
+// Refatoração da função `criaElemento` para que possua apenas a função que faça sentido ao nome. 
+
+function criaElemento(item) {  
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    numeroItem.innerHTML = item.quantidade
     novoItem.appendChild(numeroItem)
 
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
-
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
-    }
-
-    itens.push(itemAtual)
-    localStorage.setItem("item", JSON.stringify(itens))
 }
